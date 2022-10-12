d23a9b7e8b307ce1fe2106f62cc2b0285e94e795
Shruok <shroukalkalla1997@gmail.com>
2022-10-12 16:58:48 +0200

    Routing Requests To Blade Views

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@yield('title','Blog')</title>
+    <link rel="stylesheet" href="{{ asset('css/main.css') }}">
+</head>
+<body>
+
+<ul class="nav">
+    <li><a class="{{ request()->routeIs('home')? 'active' : '' }}" href="{{ route('home') }}">Home</a></li>
+    <li><a class="{{ request()->routeIs('about')? 'active' : '' }}" href="{{ route('about') }}">About</a></li>
+</ul>
+
+<div class="main">
+    @yield('content')
+</div>
+</body>
+</html>
