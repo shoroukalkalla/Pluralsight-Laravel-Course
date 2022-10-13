4c68eb21d2cf1f062f2f0851c03be207e7a5ea9c
Shruok <shroukalkalla1997@gmail.com>
2022-10-13 12:34:45 +0200

    Deleting Posts Using Route Model Binding

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -96,8 +96,12 @@ class PostController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Post $post)
     {
-        //
+        $post->delete();
+
+        return redirect()
+            ->route('home')
+            ->with('success', 'Post deleted successfully!!');
     }
 }
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -8,6 +8,11 @@
                 <h2>{{ $post->title }}</h2>
                 <p>{{ $post->description }}</p>
                 <a href="{{route('posts.edit',['post'=>$post->id])}}">Edit post</a>
+                <form method="POST" action="{{route('posts.destroy',[$post])}}">
+                    @csrf
+                    @method('DELETE')
+                    <button type="submit" class="delete">Delete post</button>
+                </form>
             </div>
         </div>
 @endsection
